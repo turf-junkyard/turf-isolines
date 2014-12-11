ecd55633afe260dc21c8f86701ffedda70d52da6
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 17:43:04 -0500

    add benching

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 //https://github.com/jasondavies/conrec.js
 //http://stackoverflow.com/questions/263305/drawing-a-topographical-map
-var tin = require('turf-tin')
-var inside = require('turf-inside')
-var grid = require('turf-grid')
-var extent = require('turf-extent')
-var planepoint = require('turf-planepoint')
-var featurecollection = require('turf-featurecollection')
-var linestring = require('turf-linestring')
-var square = require('turf-square')
+var tin = require('turf-tin');
+var inside = require('turf-inside');
+var grid = require('turf-grid');
+var extent = require('turf-extent');
+var planepoint = require('turf-planepoint');
+var featurecollection = require('turf-featurecollection');
+var linestring = require('turf-linestring');
+var square = require('turf-square');
 
 module.exports = function(points, z, resolution, breaks, done){
-  var tinResult = tin(points, z)
-  var extentBBox = extent(points)
-  var squareBBox = square(extentBBox)
-  var gridResult = grid(squareBBox, resolution)
+  var tinResult = tin(points, z);
+  var extentBBox = extent(points);
+  var squareBBox = square(extentBBox);
+  var gridResult = grid(squareBBox, resolution);
   var data = [];
 
   gridResult.features.forEach(function(pt){
     tinResult.features.forEach(function(triangle){
       if (inside(pt, triangle)) {
-        pt.properties = {}
+        pt.properties = {};
         pt.properties[z] = planepoint(pt, triangle);
       }
-    })
-  })
+    });
+  });
 
-  var depth = Math.sqrt(gridResult.features.length)
+  var depth = Math.sqrt(gridResult.features.length);
   for (var x=0; x<depth; x++){
-    var xGroup = gridResult.features.slice(x * depth, (x + 1) * depth)
-    var xFlat = []
+    var xGroup = gridResult.features.slice(x * depth, (x + 1) * depth);
+    var xFlat = [];
     xGroup.forEach(function(verticalPoint){
       if(verticalPoint.properties){
-        xFlat.push(verticalPoint.properties[z])
+        xFlat.push(verticalPoint.properties[z]);
       } else{
-        xFlat.push(0)
+        xFlat.push(0);
       }
-    })
-    data.push(xFlat)
+    });
+    data.push(xFlat);
   }
-  var interval = (squareBBox[2] - squareBBox[0]) / depth
-  var xCoordinates = []
-  var yCoordinates = []
+  var interval = (squareBBox[2] - squareBBox[0]) / depth;
+  var xCoordinates = [];
+  var yCoordinates = [];
   for (var x=0; x<depth; x++){
-    xCoordinates.push(x * interval + squareBBox[0])
-    yCoordinates.push(x * interval + squareBBox[1])
+    xCoordinates.push(x * interval + squareBBox[0]);
+    yCoordinates.push(x * interval + squareBBox[1]);
   }
 
-  var c = new Conrec
-  c.contour(data, 0, resolution, 0, resolution, xCoordinates, yCoordinates, breaks.length, breaks)
-  var contourList = c.contourList()
+  var c = new Conrec;
+  c.contour(data, 0, resolution, 0, resolution, xCoordinates, yCoordinates, breaks.length, breaks);
+  var contourList = c.contourList();
 
-  var fc = featurecollection([])
+  var fc = featurecollection([]);
   contourList.forEach(function(c){
     if(c.length > 2){
-      var polyCoordinates = []
+      var polyCoordinates = [];
       c.forEach(function(coord){
-        polyCoordinates.push([coord.x, coord.y])
-      })
-      var poly = linestring(polyCoordinates)
-      poly.properties = {}
-      poly.properties[z] = c.level
+        polyCoordinates.push([coord.x, coord.y]);
+      });
+      var poly = linestring(polyCoordinates);
+      poly.properties = {};
+      poly.properties[z] = c.level;
 
-      fc.features.push(poly)
+      fc.features.push(poly);
     }
-  })
+  });
 
   return fc;
 }
